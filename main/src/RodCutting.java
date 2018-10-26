a4480685dfe7367b546d75d425e44bff747519fb
Gerard Falco <gerardfp@gmail.com>
2018-10-26 15:26:09 +0000

    Update main/src/RodCutting.java

--- main/src/RodCutting.java
+++ main/src/RodCutting.java
@@ -1,36 +1,21 @@
 import java.util.Arrays;
 
 public class RodCutting{
+    static int cutRod(int[] prices, int size){
+        int[] maxs = new int[size+1];
 
-    static int cutRod(int price[], int n) {
-        int val[] = new int[n+1];
-        val[0] = 0;
-        int sel[] = new int[n+1];
-
-        for (int i = 1; i<=n; i++) {
-            int max_val = Integer.MIN_VALUE;
-            for (int j = 0; j < i; j++) {
-                int new_max = price[j] + val[i - j - 1];
-                if(new_max > max_val){
-                    max_val = new_max;
-                    sel[i] = j+1;
-                }
+        for (int i = 1; i < maxs.length; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < i && j < prices.length; j++) {
+                max = Math.max(max, prices[j] + maxs[i-j-1]);
             }
-            val[i] = max_val;
+            maxs[i] = max;
         }
-
-        System.out.println(Arrays.toString(val));
-        System.out.println(Arrays.toString(sel));
-        int length = n+1;
-        while(sel[length-1] > 0){
-            System.out.println(sel[length-1]);
-            length -= sel[length-1];
-        }
-        return val[n];
+        return maxs[maxs.length-1];
     }
 
-    public static void main(String args[]) {
-        int price[] = new int[] {2, 5, 8, 9, 10, 17, 17, 20};
-        System.out.println("Maximum Obtainable Value is " + cutRod(price, price.length));
+    public static void main(String[] args) {
+	    int[] prices = {1,5,6};
+        System.out.println(cutRod(prices, 8));
     }
 }
