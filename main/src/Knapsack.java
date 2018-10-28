088e8a8e8817acc3e1e15d7b13463c3bc4b82f35
gerardfp <gerardfp@gmail.com>
2018-10-29 00:17:50 +0100

    KnapSack01

--- main/src/Knapsack.java
+++ main/src/Knapsack.java
@@ -1,50 +1,44 @@
-import java.util.Arrays;
-
 public class Knapsack {
 
-    static int unbounded(int[] val, int[] wt, int W) {
-        int[] K = new int[W + 1];
+    static int unbounded(int[] values, int[] weights, int weight){
+        int[] K = new int[weight+1];
 
-        for (int i = 0; i <= W; i++) {
-            int max_val_for_item = 0;
-            for (int j = 0; j < val.length; j++) {
-                if(i >= wt[j]){
-                    int item_value = val[j];
-                    int max_with_space_left = K[i - wt[j]];
-                    max_val_for_item = Math.max(max_val_for_item, item_value + max_with_space_left);
-                }
+        for (int i = 1; i <= weight; i++) {
+            int maxvalue = Integer.MIN_VALUE; // TODO: ¿ 0 ?
+
+            for (int j = 0; j < values.length && j<i; j++) {  // TODO: ¿ j<i ?
+                if(i-weights[j] >= 0)
+                    maxvalue = Math.max(maxvalue, values[j] + K[i-weights[j]]);
             }
-            K[i] = max_val_for_item;
+
+            K[i] = maxvalue;
         }
-        System.out.println(Arrays.toString(K));
-        return K[W];
+
+        return K[K.length-1];
     }
 
-    static int knapSack01(int[] val, int[] wt, int W) {
+    static int zeroone(int[] val, int[] wt, int W) {
         int[][] K = new int[val.length+1][W+1];
 
-        for (int i = 1; i < K.length; i++) {
+        for (int i = 1; i <= val.length; i++) {
             for (int w = 1; w <= W; w++) {
                 if (wt[i-1] <= w)
                     K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
                 else
                     K[i][w] = K[i-1][w];
-                printMatrix(K);
-                System.out.println("--");
             }
         }
 
-        printMatrix(K);
         return K[K.length-1][W];
     }
 
     public static void main(String[] args) {
 	    int[] number = {4,7,3};
-	    int[] values = {2, 6, 7};
+	    int[] values = {3, 6, 7};
 	    int[] weights = {1,3,4};
 
         System.out.println(unbounded(values, weights, 4));
-        System.out.println(knapSack01(values, weights, 4));
+        System.out.println(zeroone(values, weights, 4));
     }
 
     static void printMatrix(int[][] m){
@@ -54,5 +48,6 @@ public class Knapsack {
             }
             System.out.println();
         }
+        System.out.println("----");
     }
 }
