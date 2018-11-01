6437863d8396cc9dc4b61e8bc86e7a6ffb83cf5f
carlesgm <carlesgm@gmail.com>
2018-11-01 13:36:50 +0100

    numChanges

--- carles/src/com/company/Change.java
+++ carles/src/com/company/Change.java
@@ -14,15 +14,10 @@ public class Change {
 
   private int minChange(int[] tipus, int n) {
 
-    Double val[][] = new Double[n + 1][tipus.length + 1];
+    double val[][] = new double[n + 1][tipus.length + 1];
 
     for (int i = 0; i <= n; i++) {
-      for (int j = 0; j <= tipus.length; j++) {
-        if (j == 0)
-          val[i][0] = Double.POSITIVE_INFINITY;
-        else
-          val[i][j] = (double) 0;
-      }
+      val[i][0] = Double.POSITIVE_INFINITY;
     }
 
     for (int i = 1; i <= n; i++) {
@@ -38,23 +33,17 @@ public class Change {
       }
     }
 
-    return val[n][tipus.length].intValue();
+    return (int) val[n][tipus.length];
   }
 
   private int numChanges(int[] tipus, int n) {
-    Double val[][] = new Double[n + 1][tipus.length + 1];
+    double val[][] = new double[n + 1][tipus.length + 1];
 
     //V[i,j] = MAX(V[i][j-1], 1 + V[i-t[j-1], j])
 
     for (int i = 0; i <= n; i++) {
-      for (int j = 0; j <= tipus.length; j++) {
-        if (j == 0)
-          val[i][0] = Double.NEGATIVE_INFINITY;
-        else
-          val[i][j] = (double) 0;
-      }
+      val[i][0] = Double.NEGATIVE_INFINITY;
     }
-
 
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= tipus.length; j++) {
@@ -70,6 +59,6 @@ public class Change {
       }
     }
 
-    return val[n][tipus.length].intValue();
+    return (int) val[n][tipus.length];
   }
 }
