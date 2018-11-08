0e7923959aeeb53c3286fda1921c019835650eba
gerardfp <gerardfp@gmail.com>
2018-11-08 23:30:08 +0100

    Asignacion de recursos yeah

--- main/src/Util.java
+++ main/src/Util.java
@@ -3,7 +3,29 @@ public class Util {
     static void printMatrix(int[][] m){
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
-                System.out.printf("%3d ", m[i][j]);
+                if(m[i][j] == Integer.MIN_VALUE){
+                    System.out.printf("  -");
+                }else if(m[i][j] == Integer.MAX_VALUE){
+                    System.out.printf("  +");
+                }else {
+                    System.out.printf("%3d", m[i][j]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("----");
+    }
+
+    static void printMatrix(double[][] m){
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if(m[i][j] == Double.NEGATIVE_INFINITY){
+                    System.out.printf("  -");
+                }else if(m[i][j] == Double.POSITIVE_INFINITY){
+                    System.out.printf("  +");
+                }else {
+                    System.out.printf("%3.0f", m[i][j]);
+                }
             }
             System.out.println();
         }
@@ -12,7 +34,13 @@ public class Util {
 
     static void printArray(int[] a){
         for (int i = 0; i < a.length; i++) {
-            System.out.printf("%3d", a[i]);
+            if(a[i] == Integer.MIN_VALUE){
+                System.out.printf("  -");
+            }else if(a[i] == Integer.MAX_VALUE){
+                System.out.printf("  +");
+            }else {
+                System.out.printf("%3d", a[i]);
+            }
         }
         System.out.println();
         System.out.println("----");
