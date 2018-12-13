8f6db8d6416446844a181049aaab536cdbf9338f
gerard <gerardfp@gmail.com>
2018-12-13 16:52:34 +0100

    CaminoKAristas (Dynamic OK)

--- main/src/CaminoKAristas.java
+++ main/src/CaminoKAristas.java
@@ -1,31 +1,51 @@
 public class CaminoKAristas {
+    
+    static int[][] G;
 
-    static double minKEdgesDynamic(int[][] G, int T, int V, int K){
+    static double minKEdgesDynamic(int S, int T, int K){
         double[][] D = new double[T+1][K+1];
 
-        for (int v = 0; v <= T; v++) {
-            for (int k = 0; k <= K; k++) {
-                double min = Double.POSITIVE_INFINITY;
-                for (int i = 0; i < G.length; i++) {
-                    if(G[v][i] != 0 && k-1 > 0) {
-                        min = Math.min(min, D[i][k] + G[v][i]);
+        for (int k = 0; k <= K; k++) {
+            for (int s = S; s <= T; s++) {
+                if(s==T && k==0) D[s][k] = 0;
+                else if(s!=T && k==0) D[s][k] = Double.POSITIVE_INFINITY;
+                else {
+                    double min = Double.POSITIVE_INFINITY;
+                    for (int i = 0; i < G.length; i++) {
+                        if (G[s][i] != 0 && k - 1 >= 0) {
+                            min = Math.min(min, D[i][k-1] + G[s][i]);
+                        }
                     }
+                    D[s][k] = min;
                 }
-                D[v][k] = min;
             }
         }
 
-        return D[T][K];
+        return D[0][K];
     }
 
-    static double minKEdgesRecursiu(int[][] G, int T, int V, int K){
-        if(V == T && K == 0) return 0;
-        if(V != T && K == 0) return Double.POSITIVE_INFINITY;
+    static double minKEdgesRecursiu(int S, int T, int K){
+        if(S == T && K == 0) return 0;
+        if(S != T && K == 0) return Double.POSITIVE_INFINITY;
 
         double min = Double.POSITIVE_INFINITY;
         for (int i = 0; i < G.length; i++) {
-            if(G[V][i] != 0) {
-                min = Math.min(min, minKEdgesRecursiu(G, T, i, K - 1) + G[V][i]);
+            if(G[S][i] != 0) {
+                min = Math.min(min, minKEdgesRecursiu(i, T, K - 1) + G[S][i]);
+            }
+        }
+        return min;
+    }
+
+    static double minKEdgesRecursiuLog(int S, int T, int K, int l){
+        System.out.printf("%"+l+"s%s\n", " ", "CALL " + S + "  " + K);
+        if(S == T && K == 0) return 0;
+        if(S != T && K == 0) return Double.POSITIVE_INFINITY;
+
+        double min = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < G.length; i++) {
+            if(G[S][i] != 0) {
+                min = Math.min(min, minKEdgesRecursiuLog(i, T, K - 1, l+1) + G[S][i]);
             }
         }
         return min;
@@ -33,30 +53,30 @@ public class CaminoKAristas {
 
 
     public static void main(String[] args) {
-        int[][] graph = new int[6][6];
-        graph[0][1] = 3;
-        graph[0][3] = 1;
+        G = new int[6][6];
+        G[0][1] = 3;
+        G[0][3] = 1;
 
-        graph[1][0] = 1;
-        graph[1][1] = 2;
-        graph[1][2] = 2;
-        graph[1][4] = -2;
-        graph[1][5] = 3;
+        G[1][0] = 1;
+        G[1][1] = 2;
+        G[1][2] = 2;
+        G[1][4] = -2;
+        G[1][5] = 3;
 
-        graph[2][5] = 1;
+        G[2][5] = 1;
 
-        graph[3][1] = 1;
+        G[3][1] = 1;
 
-        graph[4][3] = 2;
-        graph[4][5] = 2;
+        G[4][3] = 2;
+        G[4][5] = 2;
 
-        for (int k = 3; k < 4; k++) {
-            System.out.println(minKEdgesRecursiu(graph, 5, 0, k));
+        for (int k = 0; k < 6; k++) {
+            System.out.println(minKEdgesRecursiu(0, 5, k));
         }
         System.out.println("---");
 
-        for (int k = 3; k < 4; k++) {
-            System.out.println(minKEdgesDynamic(graph, 5, 0, k));
+        for (int k = 0; k < 6; k++) {
+            System.out.println(minKEdgesDynamic(0, 5, k));
         }
     }
 }
